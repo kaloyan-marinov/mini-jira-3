63f297b0319aca3929739c6ba81abe779d249338
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-14 23:05:21 +0200

    1: write more automated tests

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -13,4 +13,71 @@ describe('GET /api/v1/issues/:id', () => {
       message: 'Resource not found',
     });
   });
+
+  test('if ID exists, should return 200 and corresponding issue', async () => {
+    // Act.
+    const response = await request(app).get('/api/v1/issues/1');
+
+    // Assert.
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual({
+      id: 1,
+      createdAt: null,
+      status: '3 = in progress',
+      deadline: null,
+      finishedAt: null,
+      epic: 'backend',
+      description:
+        'build a backend application using Express (without a persistence layer)',
+    });
+  });
+});
+
+describe('POST /api/v1/issues', () => {
+  test('if "status" is missing, should return 400', async () => {
+    // Act.
+    const response = await request(app).post('/api/v1/issues').send({
+      description: 'containerize the backend',
+    });
+
+    // Assert.
+    expect(response.status).toEqual(400);
+    expect(response.body).toEqual({
+      message:
+        "Each of 'status', 'description' must be specified in the HTTP request's body",
+    });
+  });
+
+  test('if "description" is missing, should return 400', async () => {
+    // Act.
+    const response = await request(app).post('/api/v1/issues').send({
+      status: '1 = in backlog',
+    });
+
+    // Assert.
+    expect(response.status).toEqual(400);
+    expect(response.body).toEqual({
+      message:
+        "Each of 'status', 'description' must be specified in the HTTP request's body",
+    });
+  });
+
+  test('if "status" and "description", should return 201', async () => {
+    // Act.
+    const response = await request(app).post('/api/v1/issues').send({
+      status: '1 = in backlog',
+      description: 'containerize the backend',
+    });
+
+    // Assert.
+    expect(response.status).toEqual(201);
+    expect(response.body).toEqual({
+      id: 4,
+      createdAt: null,
+      status: '1 = in backlog',
+      deadline: null,
+      finishedAt: null,
+      description: 'containerize the backend',
+    });
+  });
 });
