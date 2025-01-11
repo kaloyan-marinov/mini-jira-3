159288d25dc96a8d5784d7119bc936c105975f56
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2025-01-11 13:33:46 +0100

    implementing and calling `revokeAccessToken`

--- scripts/issue-requests-based-on-csv-file.js
+++ scripts/issue-requests-based-on-csv-file.js
@@ -120,75 +120,96 @@ const requestsForCreatingIssues = async (
   pathToCSVFile,
   accessToken
 ) => {
-  fs.createReadStream(pathToCSVFile)
-    .pipe(csvParser())
-    .on('data', async (row) => {
-      // A JavaScript object representing the current row is stored in `row`.
-      // console.log(row);
+  const activeRequests = [];
 
-      if (!Object.keys(epicNameToEpic).includes(row['category/Epic/project'])) {
-        console.log(
-          'will not issue an HTTP request based on the current `row`'
-        );
-      } else {
-        // Sanitize `row`.
-        const sanitizedRow = { ...row };
+  const awaitAllActiveRequests = (resolve, reject) => {
+    fs.createReadStream(pathToCSVFile)
+      .pipe(csvParser())
+      .on('data', (row) => {
+        // A JavaScript object representing the current row is stored in `row`.
+        // console.log(row);
 
-        delete sanitizedRow['id'];
-
-        if (sanitizedRow['deadline'] === 'n/a') {
-          // sanitizedRow['deadline'] = null;
-          // TODO: (2024/10/23, 05:27)
-          //      update the file at `pathToCSVFile`
-          //      s.t. no Issue lacks a `deadline`
-          sanitizedRow['deadline'] =
-            sanitizedRow['finished_at'] !== 'n/a'
-              ? sanitizedRow['finished_at']
-              : new Date('1970-01-01T17:17:17');
+        if (
+          !Object.keys(epicNameToEpic).includes(row['category/Epic/project'])
+        ) {
+          console.log(
+            'will not issue an HTTP request based on the current `row`'
+          );
         } else {
-          const deadlines = sanitizedRow['deadline'].split('<<');
-          const mostRecentlySetDeadline = deadlines[0];
-          sanitizedRow['deadline'] = mostRecentlySetDeadline.trim();
+          // Sanitize `row`.
+          const sanitizedRow = { ...row };
+
+          delete sanitizedRow['id'];
+
+          if (sanitizedRow['deadline'] === 'n/a') {
+            // sanitizedRow['deadline'] = null;
+            // TODO: (2024/10/23, 05:27)
+            //      update the file at `pathToCSVFile`
+            //      s.t. no Issue lacks a `deadline`
+            sanitizedRow['deadline'] =
+              sanitizedRow['finished_at'] !== 'n/a'
+                ? sanitizedRow['finished_at']
+                : new Date('1970-01-01T17:17:17');
+          } else {
+            const deadlines = sanitizedRow['deadline'].split('<<');
+            const mostRecentlySetDeadline = deadlines[0];
+            sanitizedRow['deadline'] = mostRecentlySetDeadline.trim();
+          }
+
+          delete sanitizedRow['created_at'];
+          sanitizedRow['createdAt'] = row['created_at'];
+
+          delete sanitizedRow['finished_at'];
+          sanitizedRow['finishedAt'] =
+            row['finished_at'] === 'n/a' ? null : row['finished_at'];
+
+          sanitizedRow['parentId'] =
+            epicNameToEpic[row['category/Epic/project']];
+          delete sanitizedRow['category/Epic/project'];
+
+          console.log(
+            ['(start)', row['id'], sanitizedRow['deadline']].join(' - ')
+          );
+
+          // Create a promise for an HTTP request,
+          // whose body is set equal to `sanitizedRow`.
+          const requestPromise = (async () => {
+            try {
+              response = await fetch('http://localhost:5000/api/v1/issues', {
+                method: 'POST',
+                body: JSON.stringify(sanitizedRow),
+                headers: {
+                  'Content-Type': 'application/json',
+                  Authorization: 'Bearer ' + accessToken,
+                },
+              });
+
+              const data = await response.json();
+
+              console.log(['(final)', response.status, data._id].join(' - '));
+            } catch (err) {
+              console.log(err);
+            }
+          })();
+
+          activeRequests.push(requestPromise);
         }
+      })
+      .on('end', () => {
+        console.log('finished processing the CSV file');
 
-        delete sanitizedRow['created_at'];
-        sanitizedRow['createdAt'] = row['created_at'];
+        // Wait for all active requests to finish.
+        Promise.all(activeRequests)
+          .then(() => resolve())
+          .catch(reject);
+      })
+      .on('error', () => {
+        // Handle errors in the stream.
+        reject();
+      });
+  };
 
-        delete sanitizedRow['finished_at'];
-        sanitizedRow['finishedAt'] =
-          row['finished_at'] === 'n/a' ? null : row['finished_at'];
-
-        sanitizedRow['parentId'] = epicNameToEpic[row['category/Epic/project']];
-        delete sanitizedRow['category/Epic/project'];
-
-        console.log(
-          ['(start)', row['id'], sanitizedRow['deadline']].join(' - ')
-        );
-
-        // Issue an HTTP request, whose body is set equal to `sanitizedRow`.
-        let response;
-
-        try {
-          response = await fetch('http://localhost:5000/api/v1/issues', {
-            method: 'POST',
-            body: JSON.stringify(sanitizedRow),
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: 'Bearer ' + accessToken,
-            },
-          });
-
-          const data = await response.json();
-
-          console.log(['(final)', response.status, data._id].join(' - '));
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    })
-    .on('end', () => {
-      console.log('finished processing the CSV file');
-    });
+  return new Promise(awaitAllActiveRequests);
 };
 
 const path = process.argv[2];
@@ -198,20 +219,41 @@ if (!path) {
   process.exit(1);
 }
 
+const revokeAccessToken = async (accessToken) => {
+  let response;
+
+  try {
+    response = await fetch('http://localhost:5000/api/v1/tokens', {
+      method: 'DELETE',
+      headers: {
+        // 'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + accessToken,
+      },
+    });
+
+    // const data = await response.json();
+
+    // console.log([response.status, data].join(' - '));
+    console.log(['response.status', response.status].join(' - '));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 (async () => {
   const accessToken = await obtainAccessToken(
     process.env.USERNAME,
     process.env.PASSWORD
   );
+  console.log('accessToken =', accessToken);
 
   const epicNames = await determineEpicNames(path);
-  console.log(epicNames);
+  console.log('epicNames =', epicNames);
 
   const epicNameToEpic = await requestsForCreatingEpics(epicNames, accessToken);
-  console.log(epicNameToEpic);
+  console.log('epicNameToEpic =', epicNameToEpic);
 
   await requestsForCreatingIssues(epicNameToEpic, path, accessToken);
 
-  // TODO: (2024/12/16, 07:16)
-  //      issue a request that revokes the `accessToken`
+  await revokeAccessToken(accessToken);
 })();
